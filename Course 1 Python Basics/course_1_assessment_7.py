d0bd9ebd5c2e255424e427686132241308f0db46
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-03-16 15:53:05 +0500

    Update course_1_assessment_7.py

--- Course 1 Python Basics/course_1_assessment_7.py
+++ Course 1 Python Basics/course_1_assessment_7.py
@@ -7,12 +7,13 @@ Store the result in the variable `num_rainy_months`.
 In other words, count the number of items with values `> 3.0`.
 """
 rainfall_mi = "1.65, 1.46, 2.05, 3.03, 3.35, 3.46, 2.83, 3.23, 3.5, 2.52, 2.8, 1.85"
-split_rainfall = rainfall_mi.split(',')
+split_rainfall = rainfall_mi.split(",")
 num_rainy_months = int()
 for iterable in split_rainfall:
     iterable = float(iterable)
     if iterable > 3:
         num_rainy_months += 1
+
 """
 The variable `sentence` stores a string. 
 Write code to determine how many words in `sentence` start
@@ -24,7 +25,8 @@ split_sentence = sentence.split()
 same_letter_count = int()
 for word in split_sentence:
     if word[0] == word[-1:]:
-        same_letter_count +=1
+        same_letter_count += 1
+
 """
 Write code to count the number of strings in list `items` 
 that have the character `w` in it. Assign that number to the variable `acc_num`.
@@ -35,7 +37,8 @@ items = ["whirring", "wow!", "calendar", "wry", "glass", "", "llama","tumultuous
 acc_num = int()
 for element in items:
     if 'w' in element:
-        acc_num +=1 
+        acc_num += 1
+        
 """
 Write code that counts the number of words in `sentence`
 that contain either an “a” or an “e”. Store the result 
@@ -49,7 +52,7 @@ split_sentence_2 = sentence.split()
 num_a_or_e = int()
 for word in split_sentence_2:
     if 'a' in word or 'e' in word:
-        num_a_or_e +=1
+        num_a_or_e += 1
 """
 Write code that will count the number of vowels in the sentence `s` 
 and assign the result to the variable `num_vowels`. For this problem,
@@ -61,4 +64,4 @@ num_vowels = int()
 for letter in s:
     for vowel in vowels:
         if letter == vowel:
-            num_vowels +=1
+            num_vowels += 1
