c6ceae338e38aa07d7a18349fe6bdc7e00212781
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-03-16 16:19:50 +0500

    Update course_2_assessment_2.py

--- Course 2 Python Functions, Files and Dictionaries/course_2_assessment_2.py
+++ Course 2 Python Functions, Files and Dictionaries/course_2_assessment_2.py
@@ -5,7 +5,7 @@ China had 45 medals, Russia had 30 medals, and Germany had 17
 medals. Create a dictionary assigned to the variable `medal_count` 
 with the country names as the keys and the number of medals the country had as each key’s value.
 """
-medal_count = dict()
+medal_count = {}
 medal_count.update(
     {"United States": 70, "Great Britain": 38, "China": 45, "Russia": 30, "Germany": 17}
 )
