de06324b049ec6d6a4b136d2631dc2a49e6dd491
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-03-16 16:32:25 +0500

    Update course_2_assessment_7.py

--- Course 2 Python Functions, Files and Dictionaries/course_2_assessment_7.py
+++ Course 2 Python Functions, Files and Dictionaries/course_2_assessment_7.py
@@ -33,9 +33,6 @@ Below is a function, `sum`, that does not work. Change the
 function definition so the code works. The function should 
 still have a required parameter, `intx`, and an optional 
 parameter, `intz` with a defualt value of 5.
-
-def sum(intz=5, intx):
-    return intz + intx
 """
 
 
